dab86f22aede94d2f7670f33eaeada9ca322585c
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-10-05 05:01:59 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Write your code here
-const multiply=(num1, num2)=>
-{
-    return num1*=num2
-}
+
+let num1=31
+let num2=2
+
+let multiply=num1 * num2
 
 //Generating a random integer between 1 and 10
 let random=Math.floor(Math.random() * 10) + 1;
@@ -11,12 +12,9 @@ let random=Math.floor(Math.random() * 10) + 1;
 //Math.random generates a random number betweeen o and 1 (including decimals)
 //Using Math.floor and Math.random together is used to generate random integers
 
-let mod=(num3, num4) =>
-{
-    if(Number.isInteger(num3) && Number.isInteger(num4))
-    {
-        return num3%=num4
-    }
-}
+let num3=16
+let num4=6
+
+let mod=num3 % num4
 
 let max=Math.max(1,7,9,20)  //Math.max returns the highest number in a given dataset
